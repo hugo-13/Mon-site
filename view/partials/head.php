3795b80f3a83a1c321ba6be6c1c3255ddcb51024
hugo-13 <hugo.seigle03@gmail>
2022-04-25 14:29:33 +0200

    Update

--- view/partials/head.php
+++ view/partials/head.php
@@ -2,10 +2,19 @@
 
     <base href="/view/">
 
+    <!-- Réglage  -->
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <meta name="viewport" content="width=device-width, initial-scale = 1.0,maximum-scale=1.0, user-scalable=no" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0,maximum-scale=1.0, user-scalable=no" />
+
+
+    <!-- SEO -->
+    <meta name="description" content="Développeur web, web mobile en formation à Lyon et Saint-Etienne" />
+    <meta name="title" content="Hugo SEIGLE, développeur web en formation" />
+    <meta name="keywords" content="Hugo, Seigle, seigle, Hugo, hugo, developpeur, web" />
+    <meta name="author" content="Hugo SEIGLE" />
+
     <!-- css link  -->
     <link rel="stylesheet" href="../css/style.css">
 
--- view/partials/header.php
+++ view/partials/header.php
@@ -1,27 +1,38 @@
 <nav class="navbar navbar-expand-lg navbar-light" id="navbar">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="../#header"><img src="../images/logo_blc.png" alt="" width="50px" height="50px" class="logo" id="image0"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarNav">
-                    <ul class="navbar-nav justify-content-center">
-                        <li class="nav-item">
-                            <a class="nav-link line-anim" aria-current="page" href="../#header">Accueil</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../#presentation">Qui suis-je ?</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../#parcours">Mon parcours</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../#xp">Mes compétences</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="../#contact">Me contacter</a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </nav>
+    <div class="container-fluid">
+        <a class="navbar-brand" href="../#header"><img src="../images/logo_blc.png" alt="" width="50px" height="50px" class="logo" id="image0"></a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" id="nav-but" aria-label="Toggle navigation" onclick="navChange()">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link line-anim" aria-current="page" href="../#header">Accueil</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../#presentation">Qui suis-je ?</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../#parcours">Mon parcours</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../#xp">Mes compétences</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="../#contact">Me contacter</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<script>
+    function navChange() {
+        verif = document.getElementById("nav-but").getAttribute("aria-expanded");
+        if (verif == "false") {
+            document.getElementById("navbar").style.background = "#a8a8a862";
+        } else {
+            document.getElementById("navbar").style.background = "#4a4a4a";
+        }
+    }
+</script>
